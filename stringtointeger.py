f98922a4f263101c37a6f79f289a5248f2a7fb10
Anubha13kumari <anubhamuz@gmail.com>
2021-10-01 22:11:51 +0530

    added code for string to integer

---
+++ stringtointeger.py
@@ -0,0 +1,24 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+
+        ans = 0
+        sign = 1
+        index = 0
+        
+        s = s.strip() # deleting all leading spaces
+        
+        if len(s) == 0:
+            return 0
+        
+        if s[0] == '-':
+            sign = -1
+            index += 1
+        elif s[0] == '+':
+            index += 1
+        
+        while index < len(s) and s[index].isdigit():
+            ans = ans * 10 + int(s[index])
+            index += 1
+        
+        ans = min(max(sign*ans, -2**31), 2**31-1)
+        return ans
