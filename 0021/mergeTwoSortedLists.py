f19b7e917ba65ddcc39fe97dff8215ccfdb3ed06
Sriram S <sriramdav1729@gmail.com>
2021-10-04 10:19:47 +0530

    Added comments to code

--- 0021/mergeTwoSortedLists.py
+++ 0021/mergeTwoSortedLists.py
@@ -8,9 +8,12 @@ class Solution:
         cur1 = head1
         cur2 = head2
 
+        # list to store the values of the linked lists in sorted order
         newList = []
 
+        # traversing through the linked list 1 and linked list 2
         while(cur1 is not None and cur2 is not None):
+            # adding the smaller of the two values to newList
             if(cur1.val < cur2.val):
                 newList.append(cur1.val)
                 cur1 = cur1.next
@@ -18,18 +21,26 @@ class Solution:
                 newList.append(cur2.val)
                 cur2 = cur2.next
 
+        # incase there are any elements left in linked list 1
+        # we add them to mewList
         while(cur1 is not None):
             newList.append(cur1.val)
             cur1 = cur1.next
 
+        # incase there are any elements left in linked list 1
+        # we add them to mewList        
         while(cur2 is not None):
             newList.append(cur2.val)
             cur2 = cur2.next
 
+        # we create a linked list from the newList
         head = self.createList(newList)
         return head
 
     def createList(self,a):
+
+        # traverse through the list and create a linked list
+        # return head of the newly created linked list
         head = None
         tail = None
 
