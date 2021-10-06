3e993972fcdcbbd1a2269ef0ece1aba905bfe32c
Nagul vali <71317991+nagulvali555@users.noreply.github.com>
2021-10-06 13:23:22 +0530

    Update ThreeSumClosest.py
    updated format as per flake8

--- 0016/ThreeSumClosest.py
+++ 0016/ThreeSumClosest.py
@@ -1,13 +1,15 @@
 class Solution(object):
     def threeSumClosest(self, nums, target):
-      
-        # Sort the array
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
         nums.sort()
         closest_sum = 2**31-1
-        
         for i in range(len(nums)):
-            j,k = i+1, len(nums)-1
-            while j<k:
+            j, k = i+1, len(nums)-1
+            while j < k:
                 curr_sum = nums[i] + nums[j] + nums[k]
                 if curr_sum == target:
                     return curr_sum
@@ -17,5 +19,4 @@ class Solution(object):
                     j = j+1
                 else:
                     k = k-1
-                    
         return closest_sum
