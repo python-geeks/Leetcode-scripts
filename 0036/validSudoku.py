7a69d30df882e1e24a2f100d7eec570dccf0d335
Riya Menon <61626010+RiyaM16@users.noreply.github.com>
2021-10-06 14:28:19 +0530

    Update validSudoku.py

--- 0036/validSudoku.py
+++ 0036/validSudoku.py
@@ -12,7 +12,6 @@ class Solution:
                 # if the element is a number, check if its count exceeds 1.
                 if row.count(num) > 1:
                     return False
-                
         # checking if numbers are repeated in each column. If yes, return false
         for col in range(len(board)):
             temp = []
@@ -23,21 +22,18 @@ class Solution:
             # Checking if there are any duplicates. If yes, rule 2 is violated.
             if len(set(temp)) != len(temp):
                 return False
-            
         # checking if numbers are repeated in each 3 * 3 box.
-        for i in range(0, len(board)-2, 3):
-            for j in range(0, len(board[0])-2, 3):
+        for i in range(0, len(board) - 2, 3):
+            for j in range(0, len(board[0]) - 2, 3):
                 # Storing 3*3 box elements in temp
-                p = board[i][j:j+3]
-                q = board[i+1][j:j+3]
-                r = board[i+2][j:j+3]
-                temp = p+q+r
+                p = board[i][j:j + 3]
+                q = board[i + 1][j:j + 3]
+                r = board[i + 2][j:j + 3]
+                temp = p + q + r
                 # Checking if any number is repeated. If yes, return false
                 for k in temp:
                     if k != '.':
                         if temp.count(k) > 1:
                             return False
-        
         # If none of the rules are violated, return true
-        return True
-        
+        return True        
