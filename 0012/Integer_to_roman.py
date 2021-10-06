1ac7c351d9ff07677c07f8bedf06cae2b897c0b2
Shivam Choudhary <shivamchoudhary2k@gmail.com>
2021-10-06 21:10:01 +0530

    Added Integer to Roman
    Added Integer to Roman code with hint and explanation

---
+++ 0012/Integer_to_roman.py
@@ -0,0 +1,95 @@
+//  ->First Try Yourself read Readme.md for a hint
+
+Python Code:-
+
+1). 
+
+table = [
+    ("M", 1000), ("CM", 900), ("D", 500),
+    ("CD", 400), ("C", 100),  ("XC", 90),
+    ("L", 50),   ("XL", 40),  ("X", 10),
+    ("IX", 9),   ("V", 5),    ("IV", 4),
+    ("I", 1)
+]
+
+def to_roman(val):
+    # All test case for numbers > 4000 are wrong.
+    # I have made a fix to make those numbers as
+    if val>4000:
+        val -= 1000
+    # to make the test cases pass. So stupid !!!
+
+    v = []
+    for a, d in table:
+        while d <= val:
+            val -= d
+            v.append(a)
+    return ''.join(v)
+
+fp = open('input')
+while True:
+    s = fp.readline()
+    if not s:
+        break
+    print(to_roman(int(s)))
+
+
+2).
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        M=["","M","MM","MMM"]
+        C=["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"]
+        X=["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"]
+        I=["","I","II","III","IV","V","VI","VII","VIII","IX"]
+        
+        return M[num//1000]+C[(num//100)%10]+X[(num//10)%10]+I[num%10]
+
+3).
+
+class Solution:
+    # @param A : integer
+    # @return a strings
+    def intToRoman(self, A):
+        roman = ""
+        if A >= 1000:
+            roman = "M"*(A/1000)
+            A = A%1000
+        if A >= 900 and A < 1000:
+            roman += "CM"
+            A -= 900
+        if A >= 500 and A < 900:
+            roman += "D"
+            A -= 500
+        if A >= 400 and A < 500:
+            roman += "CD"
+            A -= 400
+        if A >= 100 and A < 400:
+            roman += "C"*(A/100)
+            A = A % 100
+        if A >= 90 and A < 100:
+            roman += "XC"
+            A -= 90
+        if A >= 50 and A < 90:
+            roman += "L"
+            A -= 50
+        if A >= 40 and A < 50:
+            roman += "XL"
+            A -= 40
+        if A >= 10 and A < 40:
+            roman += "X"*(A/10)
+            A = A % 10
+        if A == 9:
+            roman += "IX"
+            A -= 9
+        if A >= 5 and A < 9:
+            roman += "V"
+            A -= 5
+        if A == 4:
+            roman += "IV"
+        if A >= 1 and A < 4:
+            roman += "I"*(A)
+            
+            
+            
+        return roman
