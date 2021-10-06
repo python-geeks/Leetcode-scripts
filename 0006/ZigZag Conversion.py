c4416fbc0b94512e56f8b236c6c0b73fe9196b53
Pawan Jain <42181691+pawangeek@users.noreply.github.com>
2021-10-06 10:12:18 +0530

    Update ZigZag Conversion.py

--- 0006/ZigZag Conversion.py
+++ 0006/ZigZag Conversion.py
@@ -4,20 +4,22 @@ class Solution:
         if (numRows <= 1 or len(s) <= numRows):
             return s
         result = [""] * numRows
-        nRow = 0             # Counter for row number
-        reverse = False         # If going downwards reverse = False, else if going upwards reverse = True
+
+        # Counter for row number
+        nRow = 0
+        # If going downwards reverse = False, elif going upwards reverse = True
+        reverse = False
+
         # iterate over each character in the string
         for i in range(len(s)):
-            result[nRow] = result[nRow] + s[i]        # store characters
+            result[nRow] = result[nRow] + s[i]  # store characters
             if reverse is False:
                 nRow += 1
-                
-                
-                
             else:
                 nRow -= 1
             # change direction
             if (nRow == numRows - 1 or nRow == 0):
                 reverse = not reverse
-        return ''.join(result)      # join the characters
-    
+
+        # join the characters
+        return ''.join(result)
