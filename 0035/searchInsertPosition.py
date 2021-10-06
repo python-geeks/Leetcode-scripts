ccd39e1fedb4ff69693a37e1ad6295e0a402be8d
Sriram S <sriramdav1729@gmail.com>
2021-10-06 12:59:21 +0530

    Added code for search insert position problem

---
+++ 0035/searchInsertPosition.py
@@ -0,0 +1,24 @@
+class Solution:
+    def searchInsert(self, nums, target: int) -> int:
+        # search for target in the array => binary search
+        # if target is found return index
+        # else return start
+
+        start = 0
+        end = len(nums) - 1
+
+        mid = start + (end-start)//2
+
+        while start <= end:
+            mid = start + (end-start)//2
+
+            if nums[mid] == target:
+                return mid
+
+            elif target > nums[mid]:
+                start = mid + 1
+
+            else:
+                end = mid - 1
+                      
+        return start
