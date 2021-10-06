c4a32adbdac6b7f326ca4c8f9c53bd5f48ecc724
Nagul vali <71317991+nagulvali555@users.noreply.github.com>
2021-10-06 13:29:35 +0530

    Update ThreeSumClosest.py

--- 0016/ThreeSumClosest.py
+++ 0016/ThreeSumClosest.py
@@ -6,17 +6,17 @@ class Solution(object):
         :rtype: int
         """
         nums.sort()
-        closest_sum = 2**31-1
+        closest_sum = 2 ** 31-1
         for i in range(len(nums)):
-            j, k = i+1, len(nums)-1
+            j, k = i + 1, len(nums) - 1
             while j < k:
                 curr_sum = nums[i] + nums[j] + nums[k]
                 if curr_sum == target:
                     return curr_sum
-                if abs(curr_sum-target) < abs(closest_sum-target):
+                if abs(curr_sum - target) < abs(closest_sum - target):
                     closest_sum = curr_sum
                 if curr_sum < target:
-                    j = j+1
+                    j = j + 1
                 else:
-                    k = k-1
+                    k = k - 1
         return closest_sum
