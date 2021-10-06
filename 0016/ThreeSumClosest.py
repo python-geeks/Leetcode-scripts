02ab63acbcd934da8f3ec32d1ed68725d466e303
Nagul vali <71317991+nagulvali555@users.noreply.github.com>
2021-10-06 13:08:01 +0530

    Create ThreeSumClosest.py

---
+++ 0016/ThreeSumClosest.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+      
+        # Sort the array
+        nums.sort()
+        closest_sum = 2**31-1
+        
+        for i in range(len(nums)):
+            j,k = i+1, len(nums)-1
+            while j<k:
+                curr_sum = nums[i] + nums[j] + nums[k]
+                if curr_sum == target:
+                    return curr_sum
+                if abs(curr_sum-target) < abs(closest_sum-target):
+                    closest_sum = curr_sum
+                if curr_sum < target:
+                    j = j+1
+                else:
+                    k = k-1
+                    
+        return closest_sum
