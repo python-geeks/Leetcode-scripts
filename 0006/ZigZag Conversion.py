aca7b6bb08a9946221f2ede6bae32baf01517582
Saúl Yael Puente Ruiz <71954593+SaulPuente@users.noreply.github.com>
2021-10-05 21:52:30 -0500

    Update ZigZag Conversion.py

--- 0006/ZigZag Conversion.py
+++ 0006/ZigZag Conversion.py
@@ -1,29 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct  5 21:00:21 2021
-
-@author: saulp
-"""
-
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
         
+        # trivial solution
         if (numRows <= 1 or len(s) <= numRows ):
             return s
         
-        result = [""]*numRows
-        counter = 0
-        reverse = False
+        result = [""]*numRows 
+        counter = 0             # Counter for row number
+        reverse = False         # If going downwards reverse = False, else if going upwards reverse = True
         
+        # iterate over each character in the string
         for i in range(len(s)):
-            result[counter] = result[counter] + s[i]
+            result[counter] = result[counter] + s[i] # store characters
             
             if reverse == False:
                 counter += 1
             else:
                 counter -= 1
-                
+            # change direction    
             if (counter == numRows - 1 or counter == 0):
                 reverse = not reverse
-                
-        return ''.join(result)
+        
+        return ''.join(result) # join the characters
+    
