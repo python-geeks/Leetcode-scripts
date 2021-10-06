06a0474ebbf26e954df8a3c5f97e3415ca91d01d
Mansi Binjola <43286654+Mansi-Binjola@users.noreply.github.com>
2021-10-06 12:44:19 +0530

    updated plus_one.py

--- 0066/plus_one.py
+++ 0066/plus_one.py
@@ -3,24 +3,23 @@ class Solution(object):
         """
         :type digits: List[int]
         :rtype: List[int]
-        """        
+        """
         rem = 0
-        
         # Iterate array in reverse order
-        for i in range(len(digits)-1,-1,-1):
+        for i in range(len(digits) - 1, -1, -1):
             # If digit is 9: assign the digit[i] = 0
             if(digits[i] == 9):
                 # if digits[i] in first position i.e 0
                 if(i == 0):
                     rem = 1 #update rem
                 digits[i] = 0
-            
+
             # digit is less than 9: increment 1 in digits[i] and break the loop
             else:
-                digits[i] = digits[i]+1
+                digits[i] = digits[i] + 1
                 break
-                
+
         # if rem is 1, then add rem value to the 1 position of the digits list
         if(rem == 1):
-            digits.insert(0,rem)
+            digits.insert(0, rem)
         return(digits)
