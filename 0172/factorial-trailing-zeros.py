189de0e1b2967ed509ce7dea30c8bd03a9cd9cdc
hemant1101 <hemantmandal1101@gmail.com>
2021-10-10 22:34:53 +0530

    corrected linting

--- 0172/factorial-trailing-zeros.py
+++ 0172/factorial-trailing-zeros.py
@@ -1,11 +1,11 @@
-"""Logic: Every trailing zero comes prom a power of 10 and every 10 must contain a 5 
+"""Logic: Every trailing zero comes prom a power of 10 and every 10 must contain a 5
 so just count power of 5 gives us no. of trailing zeros."""
+
 
 class Solution:
     def trailingZeroes(self, n: int) -> int:
         a = 0
-        while ( n >= 5 ):
+        while (n >= 5):
             n //= 5
             a += n
-
         return a
