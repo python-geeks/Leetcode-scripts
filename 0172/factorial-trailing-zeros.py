545e2f05d85f17593025edc08842d0125ddd9cd6
hemant1101 <hemantmandal1101@gmail.com>
2021-10-10 20:01:40 +0530

    linting corrected

--- 0172/factorial-trailing-zeros.py
+++ 0172/factorial-trailing-zeros.py
@@ -2,7 +2,9 @@
 # so just count power of 5 gives us no. of trailing zeros.
 
 class Solution:
+
     def trailingZeroes(self, n: int) -> int:
+
         a = 0
         while(n >= 5):
             n //= 5
