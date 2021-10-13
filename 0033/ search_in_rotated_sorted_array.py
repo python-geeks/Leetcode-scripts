6e1c5b78c5a276698e5c378ace253d76ef422031
Jisan <jishananam@iut-dhaka.edu>
2021-10-13 22:03:05 +0600

    Added Readme and Implemented the Solution in python

---
+++ 0033/ search_in_rotated_sorted_array.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        low, high = 0, len(nums) - 1
+
+        while low <= high:
+
+            mid = low + ((high - low) >> 1)
+
+            if nums[mid] == target:
+                return mid
+            elif nums[low] > nums[mid]:
+                if nums[mid] < target and nums[high] >= target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            elif nums[low] <= nums[mid]:
+                if nums[low] <= target and nums[mid] > target:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+
+
+        return -1
