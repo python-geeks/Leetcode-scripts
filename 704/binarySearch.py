7adb868d973619a6e7f0203c3f264802d4be5865
Sriram S <sriramdav1729@gmail.com>
2021-10-14 08:47:23 +0530

    added code for binary search problem

---
+++ 704/binarySearch.py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, arr, target: int) -> int:
+        # simple Binary Search Algorithm
+
+        start = 0
+        end = len(arr) - 1
+
+        while start <= end:
+            mid = start + (end-start)//2
+            if arr[mid] == target:
+                return mid
+            elif target < arr[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return -1
