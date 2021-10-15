063ad609dee2bc46be4c635ec0c396ee4b172d43
allwells <aleenfestus@gmail.com>
2021-10-15 22:35:05 +0100

    Solution to issue #46
    Signed-off-by: allwells <aleenfestus@gmail.com>

--- 0027/RemoveElements.py
+++ 0027/RemoveElements.py
@@ -2,12 +2,13 @@ from typing import List
 
 
 class Solution:
-
     def removeElement(self, nums: List[int], val: int) -> int:
-        for i in nums:
-            # check if val is in nums list
-            if val in nums:
-                # removes the val from nums list
-                nums.remove(val)
+        k = 0
 
-        return nums  # returns nums list with remaining elements
+        for n in range(len(nums)):
+
+            if nums[n] != val:
+                nums[k] = nums[n]
+                k += 1
+
+        return k
