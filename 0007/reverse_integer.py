12e1d274cf112e3716b7f31be7fd4866b3830659
nhimanshujain <nhimanshujain@gmail.com>
2021-10-21 11:24:04 +0530

    Added solution to Leetcode Question - 7 | Reverse Integer

--- 0007/reverse_integer.py
+++ 0007/reverse_integer.py
@@ -1,9 +1,5 @@
 class Solution:
-    def reverse(self, x: int) -> int:
-        val = 0
-        pro = 1 if x > 0 else -1
-        x = abs(x)
-        while x:
-            val = val * 10 + x % 10
-            x //= 10
-        return val * pro  if (val <= 2 ** 31 - 1 and val >= -2 ** 31 ) else 0
+    def reverse(self, x):
+        s = str(x)
+        res = (int('-' + s[1:][::-1]) if s[0] == '-' else int(s[::-1]))
+        return (res if -2147483648 <= res <= 2147483647 else 0)
