600eddb79137084dbc6a50464b628a96d5932085
Spas Z <spaszahariev54@gmail.com>
2021-10-24 15:37:53 +0100

    adding problem solution

---
+++ 0394/decode-string.py
@@ -0,0 +1,31 @@
+class Solution:
+    
+    def extract_digit(self, i: int, s: str):
+        digit = ""
+        
+        while (s[i] is not "["):
+            digit += s[i]
+            i +=1
+        
+        # i + 1 to skip the [
+        return i + 1, digit
+    
+    def decode_word(self, i: int, s: str):
+        
+        concat = ""
+        while(i < len(s) and s[i] is not "]"):
+            if (s[i].isdigit()):
+                i, digit = self.extract_digit(i, s)
+                
+                i, word = self.decode_word(i, s)
+                concat += int(digit) * word
+            else:
+                concat += s[i]
+                i += 1
+        return i + 1, concat
+        
+    def decodeString(self, s: str) -> str:
+        
+        i, word = self.decode_word(0, s)
+        
+        return word 
