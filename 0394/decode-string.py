508eb12555a12955cf69b3e32712ccde7b7eddb6
Spas_Z <spaszahariev54@gmail.com>
2021-10-27 19:28:10 +0100

    fixing linting issues

--- 0394/decode-string.py
+++ 0394/decode-string.py
@@ -3,17 +3,17 @@ class Solution:
     def extract_digit(self, i: int, s: str):
         digit = ""
         
-        while (s[i] is not "["):
+        while (s[i] != "["):
             digit += s[i]
-            i +=1
-        
+            i += 1
+            
         # i + 1 to skip the [
         return i + 1, digit
     
     def decode_word(self, i: int, s: str):
         
         concat = ""
-        while(i < len(s) and s[i] is not "]"):
+        while(i < len(s) and s[i] != "]"):
             if (s[i].isdigit()):
                 i, digit = self.extract_digit(i, s)
                 
@@ -23,9 +23,10 @@ class Solution:
                 concat += s[i]
                 i += 1
         return i + 1, concat
-        
+    
     def decodeString(self, s: str) -> str:
         
         i, word = self.decode_word(0, s)
         
-        return word 
+        return word
+    
