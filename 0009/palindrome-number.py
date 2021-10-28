f821d965a9f98aa97922ac9d458f000d67a38cbc
Sanu <shawsanu46@gmail.com>
2021-10-28 19:12:18 +0530

    linting correctede

--- 0009/palindrome-number.py
+++ 0009/palindrome-number.py
@@ -1,16 +1,12 @@
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        
+    def isPalindrome(self, x: int) -> bool: 
         temp = x
         rev = 0
-        
         while( x > 0 ):
-            
             dig = x % 10  
             rev = rev * 10 + dig  
-            x = x // 10
-            
+            x = x // 10 
         if ( temp == rev ):  
             return True
-        else:  
+        else: 
             return False
